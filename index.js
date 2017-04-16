c0a0f1b2f64c41b7fb00e3fc9f65fa66eb9fbca9
Vladimir Rodkin <mail@vovanr.com>
2017-04-16 22:24:24 +0300

    Generate sound buttons; Add styles; Move sounds to sounds dir
    * Close #6, Close #4, Close #3

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 /* global document fetch soundManager */
 /* eslint arrow-parens: "off" */
 
-const getElements = (elementName) => () => document.querySelectorAll(elementName)
-const getButtonElements = getElements('button')
+const SOUND_LIST_URL = 'sounds/sound-list.json'
 
-const addListener = (eventName) => (eventHandler) => (element) => element.addEventListener(eventName, eventHandler)
-const addClickListener = addListener('click')
+const addEventListener = (eventName) => (eventHandler) => (element) => element.addEventListener(eventName, eventHandler)
+const addClickEventListener = addEventListener('click')
 
-const getSoundByName = (soundName, soundList) => (soundList[soundName])
-
-const onClick = (soundList) => (event) => {
-  soundManager.createSound({
-    url: getSoundByName(event.target.name, soundList),
-    autoPlay: true
-  })
+const getContainer = () => document.getElementById('app')
+const createButtonStack = () => document.createDocumentFragment()
+const createElement = (elementName) => document.createElement(elementName)
+const createButtonElement = () => {
+  const button = createElement('button')
+  button.setAttribute('type', 'button')
+  return button
+}
+const enableSoundButton = (button) => () => {
+  button.disabled = false
+}
+const preloadSound = ({id, url, onload}) => soundManager.createSound({id, url, onload, autoLoad: true})
+const playSound = (event) => soundManager.play(event.target.name)
+const addClickPlaySoundEventListener = addClickEventListener(playSound)
+const createSoundButton = (sound) => {
+  const {id, name, url} = sound
+  const button = createButtonElement()
+  button.textContent = name
+  button.setAttribute('name', id)
+  button.disabled = true
+  const onload = enableSoundButton(button)
+  preloadSound({id, url, onload})
+  addClickPlaySoundEventListener(button)
+  return button
+}
+const appendElementTo = (container) => (element) => container.appendChild(element)
+const appendElementArrayTo = (container) => (elementArray) => elementArray.forEach(appendElementTo(container))
+const appendSoundButtonsTo = (buttonStack) => (soundButtons) => {
+  appendElementArrayTo(buttonStack)(soundButtons)
+  return buttonStack
 }
 
 const responseToJSON = (response) => response.json()
-const fetchSoundList = () => fetch('sound-list.json').then(responseToJSON).catch(console.error)
-
-const buttons = getButtonElements()
-const forEachButton = (addClickEventListener) => buttons.forEach(addClickEventListener)
+const fetchSoundList = () => fetch(SOUND_LIST_URL).then(responseToJSON).catch(console.error)
 
 fetchSoundList()
-  .then(onClick)
-  .then(addClickListener)
-  .then(forEachButton)
+  .then(soundList => soundList.sounds)
+  .then(sounds => sounds.map(createSoundButton))
+  .then(appendSoundButtonsTo(createButtonStack()))
+  .then(appendElementTo(getContainer()))
